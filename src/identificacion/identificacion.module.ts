bb00afaccf72b4c672c764b938f3270a1d1f8d0d
Ivonne Mariana Cuncanchun Bernal <i.cuncanchunb@uniandes.edu.co>
2026-08-23 11:40:28 -0500

    module ts y providers listo

--- src/identificacion/identificacion.module.ts
+++ src/identificacion/identificacion.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { DatabaseModule } from '../datasources/database.module';
+
+import { TiendaRepository } from './repositories/tienda.repository';
+import { repositoryProviders } from './repositories/repository.providers';
+
+@Module({
+  imports: [DatabaseModule],
+
+  providers: [
+    ...repositoryProviders,
+    TiendaRepository,
+  ],
+
+  exports: [
+    TiendaRepository,
+  ],
+})
 export class IdentificacionModule {}
